834568a5f182e81796e58a7df623df08a3ba6ae8
maidakosser04 <maidabutt2004@gmail.com>
2025-07-31 10:59:56 +0500

    final push

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,10 @@
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import "./Footer.scss";
-import { Fade } from "react-reveal";
+import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-  const { isDark } = useContext(StyleContext);
+  const {isDark} = useContext(StyleContext);
 
   const currentYear = new Date().getFullYear();
 
@@ -12,10 +12,9 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-        Made with ❤️ by Maida Kosser(MK) © {currentYear} All rights reserved.
+          Made with ❤️ by Maida Kosser(MK) © {currentYear} All rights reserved.
         </p>
       </div>
     </Fade>
   );
 }
-
